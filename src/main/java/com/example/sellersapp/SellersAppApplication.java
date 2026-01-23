a558c7fd0942abc41fea11ff612794065584e5cd
SAMBERENO <arek4@spoko.pl>
2026-01-23 17:13:41 +0100

    Initial commit

---
+++ src/main/java/com/example/sellersapp/SellersAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sellersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellersAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sellersapp/SellersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sellersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellersAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
